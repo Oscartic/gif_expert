b9839905c1b3042a9f6456f620ba77371cb2ed2e
OscarB <oscartic@gmail.com>
2020-09-28 22:07:55 -0300

    add docs

---
+++ docs/precache-manifest.c9ca32d7162e431ee2c9e2c724c10d5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ace67d06be76f4cf057be584b3e598f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f17355973857b339a7a",
+    "url": "/static/css/main.a913cbe3.chunk.css"
+  },
+  {
+    "revision": "92a5aaf936af7a3a2930",
+    "url": "/static/js/2.7c8d2b56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7c8d2b56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f17355973857b339a7a",
+    "url": "/static/js/main.759c90de.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
